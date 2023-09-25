b1e66955d65fa9074bda9bd92ddeab2cf8e71801
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2023-09-25 16:25:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import axios from 'axios'; 
+
+function App() {
+  const [formData, setFormData] = useState({
+    title: '',
+    firstName: '',
+    surname: '',
+    email: '',
+    password: '',
+    type: '',
+    mobileNumber: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/users', formData); // Replace with your actual API endpoint URL
+
+      if (response.status === 200 || response.status === 201) {
+        // Registration successful, you can redirect the user or show a success message.
+        console.log('Registration successful');
+      } else {
+        // Registration failed, handle error (e.g., display error message).
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      // Handle network or other errors
+      console.error('Error:', error);
+    }
+  };
+
+
+  return (
+    <div>
+      <h2>Registration Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="surname">Surname:</label>
+          <input
+            type="text"
+            id="surname"
+            name="surname"
+            value={formData.surname}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="type">Type:</label>
+          <input
+            type="text"
+            id="type"
+            name="type"
+            value={formData.type}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="mobileNumber">Mobile Number:</label>
+          <input
+            type="text"
+            id="mobileNumber"
+            name="mobileNumber"
+            value={formData.mobileNumber}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
